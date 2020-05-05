9576c4d1f53fbfe2f30dcf2bc18fbe21de5677ca
Alexander <rodger6699@gmail.com>
2020-05-05 23:05:33 +0300

    Обновил IStory, добавил описание

--- src/project/marketplace/IStory.java
+++ src/project/marketplace/IStory.java
@@ -1,6 +1,9 @@
 package project.marketplace;
 
 public interface IStory {
+    /**
+     * Интерфейс отвечает за получение истории сделок
+     * */
     void getCurrentDeal();//текущие сделки
     void getDoneDeal();//завершенные сделки
 }
